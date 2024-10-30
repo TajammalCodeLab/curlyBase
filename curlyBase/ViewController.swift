45034f0220ba33d46c49ba2c8bf758b64b59d9fb
SID <managebydev@gmail.com>
2024-10-31 02:35:05 +0500

    first commit

---
+++ curlyBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  curlyBase
+//
+//  Created by SID on 31/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
